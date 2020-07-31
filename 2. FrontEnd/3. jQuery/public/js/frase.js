2982055508e5f71c78b87dda67f26ad5a66bd122
Kadu Lemos <kadulemos@hotmail.com>
2020-07-31 13:30:29 -0300

    #3 jQuery2 | Handling errors and improving the experience
    -> .fail()
    -> .always()

--- 2. FrontEnd/3. jQuery/public/js/frase.js
+++ 2. FrontEnd/3. jQuery/public/js/frase.js
@@ -1,7 +1,18 @@
 $('#botão-frase').click(fraseAleatoria);
 
 function fraseAleatoria() {
-    $.get('http://localhost:3000/frases');
+    $('#spinner').toggle(); // novo, mostrando o spinner
+
+    $.get('http://localhost:3000/frases', trocaFraseAleatoria)
+    .fail(function(){
+        $('#erro').toggle(); // ao falhar mostra a mensagem de erro
+        setTimeout(function(){
+            $('#erro').toggle();
+        }1500);
+    });
+    .always(function(){ // novo, escondendo o spinner
+        $('#spinner').toggle();
+    })
 }
 
 function trocaFraseAleatoria(data) {
