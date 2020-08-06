a1639425caf312a736c13f0405c977d7b3014516
Kadu Lemos <kadulemos@hotmail.com>
2020-08-06 14:02:53 -0300

    #1 Java | double

---
+++ 3. Java/FirstSteps/TestePontoFlutuante.java
@@ -0,0 +1,10 @@
+public class TestePontoFlutuante {
+    public static void main(String[] args) {
+        double salario = 1250.70;
+
+        System.out.println("O salário é: " + salario);
+
+        double divisao = 5.0 / 2;
+        System.out.println(divisao);
+    }
+}
