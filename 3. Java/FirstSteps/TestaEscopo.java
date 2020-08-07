84f313c54d8bc236562beb527ead92a55d78324c
Kadu Lemos <kadulemos@hotmail.com>
2020-08-07 12:17:38 -0300

    #1 Java | Escopo

---
+++ 3. Java/FirstSteps/TestaEscopo.java
@@ -0,0 +1,21 @@
+public class TestaEscopo {
+    public static void main(String[] args) {
+        System.out.println("Testando Condicionais #3");
+
+        int idade = 20;
+        int quantidadePessoas = 3;
+        boolean acompanhado;
+        
+        if (quantidadePessoas >= 2) {
+            acompanhado = true;
+        } else {
+            acompanhado = false;
+        }
+
+        if (idade >= 18 || acompanhado) {
+            System.out.println("Seja Bem Vindo!");
+        } else {
+            System.out.println("Infelizmente você não pode entrar");
+        }
+    }    
+}
