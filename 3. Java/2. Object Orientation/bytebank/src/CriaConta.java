7e0596d6ea397651c68bf82aadae355de0b1e678
Kadu Lemos <kadulemos@hotmail.com>
2020-08-08 12:36:26 -0300

    #1 Java | References
    primeiraConta
    segundaConta

---
+++ 3. Java/2. Object Orientation/bytebank/src/CriaConta.java
@@ -0,0 +1,16 @@
+public class CriaConta {
+    public static void main(String[] args) {
+        Conta primeiraConta = new Conta();
+        primeiraConta.saldo = 200;
+        System.out.println(primeiraConta.saldo);
+
+        primeiraConta.saldo += 100;
+        System.out.println(primeiraConta.saldo);
+
+        Conta segundaConta = primeiraConta; // verificando que primeiraConta e segundaConta sãos apenas referências
+        segundaConta.saldo = 50;
+
+        System.out.println("primeira conta tem " + primeiraConta.saldo);
+        System.out.println("segunda conta tem " + segundaConta.saldo);
+    }
+}
